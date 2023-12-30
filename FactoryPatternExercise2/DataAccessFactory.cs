221aaf8744966f526c62e99a48d9a42478ec64de
Kim Sedlacek <sedlaceks@optonline.net>
2023-12-29 22:03:37 -0500

    factory pattern ex2

---
+++ FactoryPatternExercise2/DataAccessFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    public static class DataAccessFactory
+    {
+        public static IDataAccess GetDataAccessType(string databaseType)
+        {
+             switch (databaseType)
+            {
+                case "List":
+                case "list":
+                    return new ListDataAccess();
+                case "SQL":
+                case "sql":
+                    return new SQLDataAccess();
+                case "Mongo":
+                case "mongo":
+                    return new MongoDataAccess();
+                default:
+                    return new ListDataAccess();
+                    
+            }
+        }
+    }
+}
--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What database would you like to use? Please select List, SQL or Mongo");
+            var userAnswer = Console.ReadLine();
+
+            var dataType = DataAccessFactory.GetDataAccessType(userAnswer);
+
+            var products = dataType.LoadData();
+            dataType.SaveData();
         }
+
     }
 }
